078b35097c035fc3071d1f044544511e78ef4adc
edgeidr <edge.idr@gmail.com>
2023-12-06 20:23:19 +0800

    EInput redesign

--- frontend/src/components/input/Input.vue
+++ frontend/src/components/input/Input.vue
@@ -1,11 +1,8 @@
 <template>
 	<div
-		@focus="input_ref.focus()"
 		:class="['input-wrapper', validity_class]"
 		tabindex="-1">
-		<label
-			class="input-label"
-			:for="input_ref">
+		<label class="input-label">
 			{{ props.label }}
 		</label>
 
@@ -13,13 +10,12 @@
 			v-bind="$attrs"
 			class="input"
 			:value="props.modelValue"
-			@input="$emit('update:modelValue', $event.target.value)"
-			ref="input_ref" />
+			@input="$emit('update:modelValue', $event.target.value)" />
 	</div>
 </template>
 
 <script setup>
-	import { computed, ref } from 'vue';
+	import { computed } from 'vue';
 
 	defineOptions({ inheritAttrs: false });
 	const props = defineProps({
@@ -27,8 +23,6 @@
 		modelValue: String,
 		valid: Boolean
 	});
-
-	const input_ref = ref(null);
 
 	const validity_class = computed(() => {
 		if (!props.modelValue) return null;
