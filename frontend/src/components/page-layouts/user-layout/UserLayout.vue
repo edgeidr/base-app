315575ef85c7bb77fe4a2f86600fb5029ebcd8a1
edgeidr <edge.idr@gmail.com>
2024-01-20 20:40:53 +0800

    feat: [UserLayout] component

--- frontend/src/components/page-layouts/user-layout/UserLayout.vue
+++ frontend/src/components/page-layouts/user-layout/UserLayout.vue
@@ -1,3 +1,11 @@
 <template>
-	<router-view></router-view>
+	<EHeader />
+	<EBody />
+	<EFooter />
 </template>
+
+<script setup>
+	import EHeader from '../../app-shell/header/Header.vue';
+	import EBody from '../../app-shell/body/Body.vue';
+	import EFooter from '../../app-shell/footer/Footer.vue';
+</script>
