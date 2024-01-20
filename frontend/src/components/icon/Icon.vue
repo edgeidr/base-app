cff6f82cfaba37cd94880a774241c9190042bfda
edgeidr <edge.idr@gmail.com>
2024-01-20 20:40:09 +0800

    feat: [Icon] component

---
+++ frontend/src/components/icon/Icon.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="icon-wrapper">
+		<component
+			:is="icon"
+			:class="['icon', color_class, size_class]" />
+	</div>
+</template>
+
+<script setup>
+	import { computed, defineAsyncComponent } from 'vue';
+
+	const props = defineProps({
+		name: String,
+		color: String,
+		size: String
+	});
+
+	const icon = defineAsyncComponent(() => {
+		return import(`../../assets/icons/${props.name}.svg`);
+	});
+
+	const color_class = computed(() => {
+		if (!props.color) return;
+		return `icon--${props.color}`;
+	});
+
+	const size_class = computed(() => {
+		if (!props.size) return;
+		return `icon--${props.size}`;
+	});
+</script>
+
+<style lang="scss">
+	@import './Icon.scss';
+</style>
--- frontend/src/plugins/custom-components.js
+++ frontend/src/plugins/custom-components.js
@@ -2,10 +2,12 @@ import Card from '../components/card/Card.vue';
 import Input from '../components/input/Input.vue';
 import Button from '../components/button/Button.vue';
 import Link from '../components/link/Link.vue';
+import Icon from '../components/icon/Icon.vue';
 
 export default function register_custom_components(app) {
 	app.component('ECard', Card);
 	app.component('EInput', Input);
 	app.component('EButton', Button);
 	app.component('ELink', Link);
+	app.component('EIcon', Icon);
 }
