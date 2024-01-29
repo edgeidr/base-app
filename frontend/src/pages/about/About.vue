92a50951c3ed52d1ef38a625adfb4e86c2777654
edgeidr <edge.idr@gmail.com>
2024-01-29 23:14:24 +0800

    feat (About): About page

--- frontend/src/pages/about/About.vue
+++ frontend/src/pages/about/About.vue
@@ -1,3 +1,58 @@
-<template>test</template>
+<template>
+	<section class="[ wrapper h-auto py-section ][ section-about ]">
+		<div class="about-wrapper">
+			<h1 class="about-title mb-lg">About</h1>
 
-<script setup></script>
+			<div class="[ about-content ][ flow ]">
+				<img
+					:src="about"
+					alt=""
+					class="about-image" />
+				<p>
+					Knotjam is a vibrant crochet clothing and accessories brand that embodies the art of slow fashion. We believe in creating unique, handmade pieces that celebrate individuality and
+					showcase the timeless beauty of crochet craftsmanship. With a focus on sustainability, ethics, and supporting local artisans, we are dedicated to making a positive impact on both
+					fashion and the environment.
+				</p>
+				<h5 class="text-md f-body">Meet Jam Gascon, Founder and Maker</h5>
+				<p class="mt-2xs">
+					At the heart of Knotjam is our founder and maker, Jam Gascon. Jam's passion for crochet began at a young age, where she discovered the joy of transforming yarn into wearable works
+					of art. With a keen eye for detail and a deep appreciation for sustainable fashion, Jam embarked on a journey to create a brand that represents her values and allows others to
+					embrace the artistry of crochet.
+				</p>
+				<p>
+					Driven by her commitment to slow fashion, Jam meticulously handcrafts each piece in our collection, infusing them with love and care. Her creative vision, combined with her
+					dedication to ethical practices and support for local artisans, has shaped Knotjam into a brand that not only offers exquisite crochet creations but also tells a story of
+					empowerment and community.
+				</p>
+				<h5 class="text-md f-body">The Knotjam Experience</h5>
+				<p class="mt-2xs">
+					At Knotjam, we believe that fashion should be a reflection of your unique personality. Our collections showcase a wide range of crochet clothing, including tops, bottoms, dresses,
+					swimwear, and accessories for all genders and ages. Each piece is thoughtfully designed to capture the latest trends while staying true to our commitment to sustainability and
+					ethical practices.
+				</p>
+				<p>
+					Customization lies at the core of Knotjam. We take pride in offering custom-made clothing, ensuring a perfect fit and a truly personalized experience for each customer. Whether
+					you're looking for a cozy hat, a stylish handbag, or a one-of-a-kind plushie, we're here to bring your crochet dreams to life.
+				</p>
+				<p>
+					Our dedication to the environment is unwavering. Knotjam is proud to embrace a circular economy by using environmentally friendly materials and avoiding the use of plastics in our
+					production process. We also strive to promote conscious consumerism, encouraging our customers to invest in quality, handmade pieces that will stand the test of time.
+				</p>
+				<p>
+					Join us in our mission to redefine fashion. Explore our collection, immerse yourself in the beauty of crochet, and experience the joy of wearing something truly unique. With
+					Knotjam, you're not just buying fashion – you're becoming part of a community that celebrates slow fashion, supports local talent, and embraces sustainable living.
+				</p>
+				<p>Thank you for choosing Knotjam, where crochet meets creativity, and fashion becomes a statement of conscious style.</p>
+				<p>Together, let's weave a brighter future, one stitch at a time.</p>
+				<div class="mt-2xl w-fit">
+					<h3>Jam Gascon</h3>
+					<p>Founder and Maker</p>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script setup>
+	import about from '../../assets/images/sample/about.png';
+</script>
